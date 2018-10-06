0b1c252eaae58bb2fd333a1fe7a82a770ea4e6e2
Alex Sanchez <alex@Alexs-MacBook-Pro.local>
2018-10-06 17:30:58 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,41 @@
+// comments
+
+/**
+ * Awesome
+ */
+
+true;
+false;
+null;
+undefined;
+void 0;
+1;
+1.0;
+/a/;
+("a");
+`a`;
+`${a}`;
+
+2 + 4;
+4 - 2;
+4 ** 4;
+
+function awesome() {
+  console.log("hi");
+}
+
+class Console {
+  constructor() {
+    super();
+  }
+  get hi() {}
+}
+
+class Console extends Awesome {
+  constructor() {
+    super();
+  }
+  get hi() {
+    console.log("hi");
+  }
+}
